43d580c609dad25fd033018addca610d868588fc
Solofelang <solofelangseboko@gmail.com>
2022-06-18 13:27:44 +0200

    topics table

---
+++ VGC/Models/Topics.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VGC.Models
+{
+    public class Topics
+    {
+        [Key]
+        public string TopicId { get; set; }
+        public string TopicName  { get; set; }
+        public string TopicDescription { get; set; }
+
+        
+    }
+}
